1fa4a058c41b46bb1e15e9b9f8dec2dd8a3f05ac
mnshey <k216q013@stu.kyoto-seika.ac.jp>
2017-07-06 09:30:20 +0900

    initial

---
+++ Assets/atari.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class atari : MonoBehaviour {
+
+		public AudioClip audioClip1;
+    private AudioSource audioSource;
+    private ParticleSystem particle;
+
+	// Use this for initialization
+	void Start () {
+	particle = this.GetComponent<ParticleSystem> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+				audioSource = gameObject.GetComponent<AudioSource>();
+        
+
+
+}
+	void OnCollisionEnter(Collision collision){
+  			audioSource.clip = audioClip1;
+        audioSource.Play ();
+        particle.Play ();
+}
+
+}
